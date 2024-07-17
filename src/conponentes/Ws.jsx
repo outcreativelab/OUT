1d9d7b813f3e042d57559e78ad9b7689c08f58d3
ricarddjob <ricarddjob@gmail.com>
2024-07-17 15:50:04 -0400

    4

--- src/conponentes/Ws.jsx
+++ src/conponentes/Ws.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Ws = () => {
   return (
-    <div className='z-50 fixed w-[120px] h-full grid place-items-end pr-[30px] pb-[-10px] end-0 content-center
+    <div className='z-40 fixed w-[120px] h-full grid place-items-end pr-[30px] pb-[-10px] end-0 content-center 
     max-[720.05px]:pt-[340px] max-[720.05px]:pr-[10px]'>
     <div className=' w-[4rem] h-[4rem] bg-rojo rounded-full content-center grid justify-items-center drop-shadow-[0_4px_6px_rgba(0,0,0,0.40)] 
     max-[720.05px]:h-[11dvw] max-[720.05px]:w-[11dvw]'>
