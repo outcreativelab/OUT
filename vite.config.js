3ddf6b0c312536c3078c1e13c18864923aa6d8dd
ricarddjob <ricarddjob@gmail.com>
2024-07-17 16:38:16 -0400

    5

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ricarddjob.github.io/OUT",
+  base: "https://outcreativelab.github.io/OUT",
 })
